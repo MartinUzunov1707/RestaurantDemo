fac1ee4b59acced8bf03b2d91e673dc1c3362375
Kamui <Kamuicho>
2023-03-05 13:57:28 +0200

    Added RestaurantContext class.

---
+++ Data/RestaurantContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    internal class RestaurantContext : DbContext
+    {
+        public RestaurantContext() : base("name=RestaurantContext") { }
+        public DbSet<Dish> Dishes { get; set; }
+    }
+}
