8f3ec80aa5bf9f53ed5031acaafd72c956d3eb35
Kamui <Kamuicho>
2023-03-05 13:28:00 +0200

    Add Dish Model in Models folder.

---
+++ Data/Models/Dish.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    /// <summary>
+    /// Model class for the Dish. Used in Database.
+    /// </summary>
+    public class Dish
+    {
+        public int ID { get; set; }
+        public string DishName { get; set; }
+        public double DishPrice { get; set; }
+        public string DishDescription { get; set; }
+        public byte[] Image { get; set; }
+        public bool IsVegetarian { get; set; }
+        public bool IsVegan { get; set; }
+        public bool GlutenFree { get; set; }
+        public bool Kosher { get; set; }
+    }
+}
