e8ce9e8a6cce9229b30993d26685f8cb417c8998
Kamui <Kamuicho>
2023-03-05 14:13:25 +0200

    Added XML documentation to ProductBusiness class.

--- Business/RestaurantBusiness.cs
+++ Business/RestaurantBusiness.cs
@@ -7,9 +7,19 @@ using System.Threading.Tasks;
 
 namespace Business
 {
+    /// <summary>
+    /// Class that contains the business layer of the app.
+    /// </summary>
     public class RestaurantBusiness
     {
+        /// <summary>
+        /// RestaurantContext Field to ensure communication between layers.
+        /// </summary>
         private RestaurantContext RestaurantContext;
+        /// <summary>
+        /// A function that returns all the dishes in the database.
+        /// </summary>
+        /// <returns>List<Dish></returns>
         public List<Dish> GetAll()
         {
             using (RestaurantContext = new RestaurantContext())
@@ -17,13 +27,22 @@ namespace Business
                 return RestaurantContext.Dishes.ToList();
             }
         }
-        public  Dish Get(int id)
+        /// <summary>
+        /// A function that returns a Dish by a given ID.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Dish</returns>
+        public Dish Get(int id)
         {
             using(RestaurantContext = new RestaurantContext())
             {
                 return RestaurantContext.Dishes.Find(id);
             }
         }
+        /// <summary>
+        /// A function that adds a Dish to the database.
+        /// </summary>
+        /// <param name="dish"></param>
         public void Add(Dish dish)
         {
             using (RestaurantContext = new RestaurantContext())
@@ -32,6 +51,10 @@ namespace Business
                 RestaurantContext.SaveChanges();
             }
         }
+        /// <summary>
+        /// A function that updates a dish that is already in the database.
+        /// </summary>
+        /// <param name="dish"></param>
         public void Update(Dish dish)
         {
             using (RestaurantContext = new RestaurantContext())
@@ -44,6 +67,10 @@ namespace Business
                 }
             }
         }
+        /// <summary>
+        /// A function that removes a Dish from the database by ID.
+        /// </summary>
+        /// <param name="id"></param>
         public void Delete(int id)
         {
             using (RestaurantContext = new RestaurantContext())
